a8198f37df7b80cdbd740a3d7cdb65b155eed200
Andrew Clifford <andrew.clifford@axientcorp.com>
2025-09-24 09:35:00 -0700

    Fix typescript error and homepage unit test

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,14 +1,16 @@
-import Home from '../pages/home/Home';
-import { setup, screen } from './test-utils';
+import Home from "../pages/home/Home";
+import { setup, screen } from "./test-utils";
 
-describe('Home Page', () => {
-  it('renders the Home Page header', () => {
+describe("Home Page", () => {
+  it("renders the Home Page header", () => {
     setup(<Home />);
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Home Page');
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent("Dog Page");
   });
 
-  it('renders the Dog Fact subheader', () => {
+  it("renders the Dog Fact subheader", () => {
     setup(<Home />);
-    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent('Dog Fact');
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Welcome to the home page!",
+    );
   });
 });
--- src/tests/test-utils.ts
+++ src/tests/test-utils.ts
@@ -1,27 +1,28 @@
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import type { ComponentType, PropsWithChildren } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import type { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 function customRender(ui: React.ReactElement, { ...renderOptions } = {}) {
-    const Wrapper = ({ children }: PropsWithChildren) => {
-        return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-    };
+  const Wrapper: React.FC<PropsWithChildren> = ({ children }) => {
+    return React.createElement(QueryClientProvider, { client: queryClient }, children);
+  };
 
-    return render(ui, {
-        wrapper: Wrapper as ComponentType, 
-        ...renderOptions,
-    });
+  return render(ui, {
+    wrapper: Wrapper,
+    ...renderOptions,
+  });
 }
 
 function setup(jsx: React.ReactElement) {
-    return {
-        user: userEvent.setup(),
-        render: { ...customRender(jsx) },
-    };
+  return {
+    user: userEvent.setup(),
+    render: { ...customRender(jsx) },
+  };
 }
 
-export * from '@testing-library/react';
+export * from "@testing-library/react";
 export { setup };
